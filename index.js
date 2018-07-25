f92b48e86f50f0193376d320ddca81081441e739
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-25 17:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+  if (someValue < 400) {
+    return 'This one is on me!';
+  }
+  if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue >= 2500) {
+      return 'No can do.';
+  }
+
+}
+
+function ternaryCheckCity (city) {
+  let answer = city == "NYC" ? "Ok, sounds good." : "No go.";
+   return answer;  
+   }
+
+
+function switchOnCharmFromTip () {
+  switch 
+}
+
+
