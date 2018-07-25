2a45d8be9917a4c6b1cae4c59671a6e6ffc0a3b1
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-25 17:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(someValue) {
     return 'This one is on me!';
     
   }
-  else if (someValue > 2000) {
+  else if (someValue > 2000 && someValue < 2500) {
     return 'I will gladly take your thirty bucks.'; 
   }
   else if (someValue > 2500) {
