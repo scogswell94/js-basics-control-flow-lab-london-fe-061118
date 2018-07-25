b62a8f5830abcb20f7cf0318eb937a9b1b55945c
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-07-25 17:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function scuberGreetingForFeet(someValue) {
   //this is where we can use conditionals given our argument, someValue
   //don't forget to return whatever the result is!
-  if (someValue < 400) {
+  if (someValue <= 400) {
     return 'This one is on me!';
   }
   
